3bd696db333b0169c9f39c66ee139283f164f955
djm3622 <djm3622@rit.edu>
2022-03-16 10:12:25 -0400

    commit

---
+++ data_project/src/main/java/com/example/data_project/DataProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProjectApplication.class, args);
+	}
+
+}
