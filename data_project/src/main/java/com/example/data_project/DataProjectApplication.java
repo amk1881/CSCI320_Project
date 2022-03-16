70cd500ae5d2d8e958721a15f9a42481a18ba0d7
djm3622 <djm3622@rit.edu>
2022-03-16 10:31:38 -0400

    commit

--- data_project/src/main/java/com/example/data_project/DataProjectApplication.java
+++ data_project/src/main/java/com/example/data_project/DataProjectApplication.java
@@ -2,9 +2,16 @@ package com.example.data_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DataProjectApplication {
+public class DataProjectApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DataProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataProjectApplication.class, args);
