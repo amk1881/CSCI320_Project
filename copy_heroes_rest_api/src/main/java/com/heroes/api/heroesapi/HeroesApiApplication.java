10a18fbe10f42da790cccbe3cca4844bd5294438
Anna Kurchenko <amk1881@g.rit.edu>
2022-03-18 17:14:36 -0400

    merge conflicts

--- copy_heroes_rest_api/src/main/java/com/heroes/api/heroesapi/HeroesApiApplication.java
+++ copy_heroes_rest_api/src/main/java/com/heroes/api/heroesapi/HeroesApiApplication.java
@@ -2,10 +2,16 @@ package com.heroes.api.heroesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+<<<<<<< HEAD:copy_heroes_rest_api/src/main/java/com/heroes/api/heroesapi/HeroesApiApplication.java
 public class HeroesApiApplication {
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DataProjectApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HeroesApiApplication.class, args);
 	}
